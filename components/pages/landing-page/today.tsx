1e63e43764d55b95531cacb096712bb149cc1226
Nha Nguyen <nhant.guide@gmail.com>
2024-01-23 16:07:52 +0700

    chore: clean code + fix build

--- components/pages/landing-page/today.tsx
+++ components/pages/landing-page/today.tsx
@@ -1,8 +1,7 @@
 "use client";
-import SkeletonBox from "@/components/molecules/SkeletonBox";
 import useSWRAPI from "@/hooks/useApi";
 import { Box, Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { ProductTypeList } from "types";
 import MenuItem from "../../organism/FoodItem/index";
 
