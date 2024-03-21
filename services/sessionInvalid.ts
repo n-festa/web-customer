625bc971fa63ab41e6e8899a301f8d995505b3a4
Nha Nguyen <nhant.guide@gmail.com>
2024-03-21 13:33:52 +0700

    fix: handle refresh token

--- services/sessionInvalid.ts
+++ services/sessionInvalid.ts
@@ -5,19 +5,25 @@ import { setUserInfo } from "@/store/reducers/userInfo";
 import { getTokenRefresh, removeToken, removeTokenRefresh, setToken, setTokenRefresh } from "@/utils/auth";
 import { routes } from "@/utils/routes";
 import apiServices from "./sevices";
+let isHandleRefreshToken = false;
 
 export const handleRefreshToken = async (errDest?: string): Promise<string | undefined> => {
+    if (isHandleRefreshToken) return;
     removeToken();
     const refresh_token = getTokenRefresh();
+
     if (refresh_token) {
+        isHandleRefreshToken = true;
         const res = await apiServices.requestToken(refresh_token);
         if (res?.data) {
             const { access_token, refresh_token } = res.data;
             setToken(access_token);
+            console.log("TEETSTS", refresh_token);
             setTokenRefresh(refresh_token);
 
             return Promise.resolve(access_token);
         }
+        isHandleRefreshToken = false;
     }
     store.dispatch(setUserInfo(undefined));
     removeTokenRefresh();
